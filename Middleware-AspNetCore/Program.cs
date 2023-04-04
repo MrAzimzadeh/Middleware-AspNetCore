09387da0d4903d5b82abcfc487a8c6af90250056
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-04-04 15:33:06 +0400

    add use middleware && start custom middleware

--- Middleware-AspNetCore/Program.cs
+++ Middleware-AspNetCore/Program.cs
@@ -20,10 +20,37 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.Use(async (context, next) =>
+    {
+        Console.WriteLine("Start use midlaware");
+        await next.Invoke(); //  ozunden sonra gelen middlaware kecmek uccun istifa de olunur ]
+        Console.WriteLine("Stop use midlaware");
+
+    });
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Start use midlaware");
+    await next.Invoke(); //  ozunden sonra gelen middlaware kecmek uccun istifa de olunur ]
+    Console.WriteLine("Stop use midlaware");
+
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Start use midlaware");
+    await next.Invoke(); //  ozunden sonra gelen middlaware kecmek uccun istifa de olunur ]
+    Console.WriteLine("Stop use midlaware");
+
+});
+
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+
+app.Run(/*async context => { Console.WriteLine("Run midlaware"); }*/); // middleware leri sonlandrirmaq ucun istifade olunur 
+
